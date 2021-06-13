f756712af18c87cb6e63b7dbba7ebca296bc4fd0
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2021-06-13 13:27:24 -0400

    Added Buttons and Headers

--- todo-list/src/app/app.module.ts
+++ todo-list/src/app/app.module.ts
@@ -2,10 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {HeaderComponent} from './components/header/header.component';
+import {ButtonComponent } from './components/button/button.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ButtonComponent
+
   ],
   imports: [
     BrowserModule
@@ -13,4 +18,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
