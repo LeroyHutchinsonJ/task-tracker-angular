39deb996685f9feb28345c687bdb3d64d4bcef55
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2021-06-14 06:10:32 -0400

    Created the Task Interface

--- todo-list/src/app/components/button/button.component.ts
+++ todo-list/src/app/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -7,12 +7,24 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ButtonComponent implements OnInit {
 
+  // @Input allows me to take an input as a parameter when calling this component
   @Input() text: string;
   @Input() color: string;
+
+  @Output()
+  btnClick = new EventEmitter();
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+
+  // the onClick method is the method I call in the button component when it is clicked
+  onClick(): void {
+    //this tells the parent class that this button has been clicked
+    this.btnClick.emit();
+  }
+
 
 }
